96f265328ed3ec81c8ec87990d6fba985efb3283
ThreeDaws <nazarmartyn2000@gmail.com>
2020-04-04 16:10:54 +0300

    Migrations

--- quiz/migrations/0001_initial.py
+++ quiz/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, verbose_name='Test')),
                 # ('time', models.TimeField()),
-                ('time', models.PositiveIntegerField()),
+                ('time', models.IntegerField()),
                 ('url', models.SlugField(max_length=160, unique=True)),
                 ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quiz.Category', verbose_name='Category')),
             ],
