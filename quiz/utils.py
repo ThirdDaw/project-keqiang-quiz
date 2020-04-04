b276eccc83c8f31c1ad79a665b0271b0c9a52a93
ThreeDaws <nazarmartyn2000@gmail.com>
2020-04-04 13:23:32 +0300

    Changes in email-sending

--- quiz/utils.py
+++ quiz/utils.py
@@ -1,38 +1,41 @@
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
-# import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 import datetime
 
 
-# msg = MIMEMultipart()
-#
-# message = "Thank you"
-#
-# # setup the parameters of the message
-# password = "worgen2000"
-# msg['From'] = "nazariimartyniuk2000@gmail.com"
-# msg['To'] = "nazarmartyn2000@gmail.com"
-# msg['Subject'] = "Subscription"
-#
-# msg.attach(MIMEText(message, 'plain'))
-#
-# server = smtplib.SMTP('smtp.gmail.com: 587')
-#
-# server.starttls()
-#
-# server.login(msg['From'], password)
-#
-# server.sendmail(msg['From'], msg['To'], msg.as_string())
-#
-# server.quit()
-#
-# print("successfully sent email to %s:" % (msg['To']))
+def send_email_with_results(result):
+    msg = MIMEMultipart()
+
+    message = str(result)
+
+    # setup the parameters of the message
+    password = "password"
+    msg['From'] = "email"
+    msg['To'] = "nazarmartyn2000@gmail.com"
+    msg['Subject'] = "Test Results"
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+
+    server.starttls()
+
+    server.login(msg['From'], password)
+
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+    print("successfully sent email to %s:" % (msg['To']))
 
 
 def save_result_in_file(result):
     filename = str(datetime.datetime.now().isoformat()).replace(":", " ")
     filepath = "media/results/" + filename
+
     print(filename)
+
     f = open(filepath, "w")
     f.write(str(result))
     f.close()
--- quiz/views.py
+++ quiz/views.py
@@ -54,11 +54,16 @@ def test_result(request, slug, next_slug):
 
     result = right_answers_counter / question_counter
 
+    message = "Results for test " + current_test.name + ". \nStudent got right " + str(round(
+        result * 100)) + "% of the tasks. \n" + str(right_answers_counter) + " of " + str(
+        question_counter) + " is correct."
+
     context = {
         "category_list": Category.objects.all(),
-        "result": result
+        "result": message
     }
 
-    save_result_in_file(result)
+    save_result_in_file(message)
+    send_email_with_results(message)
 
     return render(request, "quiz/test_result.html", context=context)
